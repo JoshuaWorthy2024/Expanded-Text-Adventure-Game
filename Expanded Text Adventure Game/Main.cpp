37f2d2c541b39ea32ef7e91c94d624746cf15d83
JoshuaWorthy2024 <joshuaworthy@cgspectrum.institute>
2025-02-08 02:04:52 -0600

    Setup player creation with name and few basic validation checks

--- Expanded Text Adventure Game/Main.cpp
+++ Expanded Text Adventure Game/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using std::cout;
 using std::cin;
@@ -11,22 +12,54 @@ const string SavePath = "PlayerSave.txt";
 void SavePlayerData();
 bool ReadPlayerData();
 void PrintPlayerData();
+bool HasInvalidSpaces(std::string EnteredString);
 
 bool IsRunning = true;
 
 // Player Stats
+std::string gPlayerName = "";
 int gPlayerHealth = 100;
 int gPlayerMana = 100;
 int gPlayerGold = 0;
 int gPlayerLevel = 1;
 int gPlayerExperience = 0;
-int gAttackPower = 10;
-int gDefensePower = 10;
+int gPlayerAttackPower = 10;
+int gPlayerDefencePower = 10;
+
+
+// key terms
+const int SAVE_NAME_KEY = 0;
+const int SAVE_HEALTH_KEY = 1;
+const int SAVE_MANA_KEY = 2;
+const int SAVE_GOLD_KEY = 3;
+const int SAVE_LEVEL_KEY = 4;
+const int SAVE_EXPERIENCE_KEY = 5;
+const int SAVE_ATTACK_POWER_KEY = 6;
+const int SAVE_DEFENCE_POWER_KEY = 7;
 
 int main() 
 {
-	if (!ReadPlayerData()) 
+	if (!ReadPlayerData())
 	{
+		bool EnteredValidName = false;
+		//cout << "Please press enter to continue." << endl;
+		//cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Flush buffer
+		while (!EnteredValidName)
+		{
+			cout << "Enter your character's name: ";
+			std::getline(cin, gPlayerName);
+			if (gPlayerName.size() > 12) {
+				cout << "You cannot enter a name that exceeds 12 characters.";
+			}
+			else
+			{
+				if (!HasInvalidSpaces(gPlayerName)) {
+					EnteredValidName = true;
+				}
+			}
+			cout << endl << endl;
+		}
+
 		SavePlayerData();
 	}
 	
@@ -44,14 +77,16 @@ void SavePlayerData()
 
 	if (SaveFile.is_open()) 
 	{
-		SaveFile << gPlayerHealth << endl;
-		SaveFile << gPlayerMana << endl;
-		SaveFile << gPlayerGold << endl;
-		SaveFile << gPlayerLevel << endl;
-		SaveFile << gPlayerExperience << endl;
-		SaveFile << gAttackPower << endl;
-		SaveFile << gDefensePower << endl;
+		SaveFile << SAVE_NAME_KEY << ": " << gPlayerName << endl;
+		SaveFile << SAVE_HEALTH_KEY << ": " << gPlayerHealth << endl;
+		SaveFile << SAVE_MANA_KEY << ": " << gPlayerMana << endl;
+		SaveFile << SAVE_GOLD_KEY << ": " << gPlayerGold << endl;
+		SaveFile << SAVE_LEVEL_KEY << ": " << gPlayerLevel << endl;
+		SaveFile << SAVE_EXPERIENCE_KEY << ": " << gPlayerExperience << endl;
+		SaveFile << SAVE_ATTACK_POWER_KEY << ": " << gPlayerAttackPower << endl;
+		SaveFile << SAVE_DEFENCE_POWER_KEY << ": " << gPlayerDefencePower << endl;
 		SaveFile.close();
+
 		PrintPlayerData();
 	}
 	else
@@ -71,18 +106,100 @@ bool ReadPlayerData()
 		return false;
 	}
 
-	cout << "Reading player data...";
+	cout << "Reading player data..." << endl << endl;
+	std::string line = "";
+	std::string key = "";
+	std::string value = "";
+
+	while (SaveFile)
+	{
+			std::getline(SaveFile, line);
+
+			if (!line.empty()) {
+				int position = line.find(':');
+				key = line.substr(0, position);
+				value = line.substr(position + 2);
+
+				switch (std::stoi(key))
+				{
+				case SAVE_NAME_KEY:
+				{
+					gPlayerName = value;
+					break;
+				}
+				case SAVE_HEALTH_KEY:
+				{
+					gPlayerHealth = std::stoi(value);
+					break;
+				}
+				case SAVE_MANA_KEY:
+				{
+					gPlayerMana = std::stoi(value);
+					break;
+				}
+				case SAVE_GOLD_KEY:
+				{
+					gPlayerGold = std::stoi(value);
+					break;
+				}
+				case SAVE_LEVEL_KEY:
+				{
+					gPlayerLevel = std::stoi(value);
+					break;
+				}
+				case SAVE_EXPERIENCE_KEY:
+				{
+					gPlayerExperience = std::stoi(value);
+					break;
+				}
+				case SAVE_ATTACK_POWER_KEY:
+				{
+					gPlayerAttackPower = std::stoi(value);
+					break;
+				}
+				case SAVE_DEFENCE_POWER_KEY:
+				{
+					gPlayerDefencePower = std::stoi(value);
+					break;
+				}
+				default:
+				{
+					cout << "-Unknown key placed: " << key << endl;
+					break;
+				}
+				}
+			}
+	}
+	PrintPlayerData();
 	// load data etc
 	return true;
 }
 
 void PrintPlayerData() 
 {
+	cout << "Name: " << gPlayerName << endl;
 	cout << "Health: " << gPlayerHealth << endl;
 	cout << "Mana: " << gPlayerMana << endl;
 	cout << "Gold: " << gPlayerGold << endl;
 	cout << "Level: " << gPlayerLevel << endl;
 	cout << "Experience: " << gPlayerExperience << endl;
-	cout << "Attack: " << gAttackPower << endl;
-	cout << "Defense: " << gDefensePower << endl << endl;
+	cout << "Attack: " << gPlayerAttackPower << endl;
+	cout << "Defense: " << gPlayerDefencePower << endl << endl;
+}
+
+bool HasInvalidSpaces(std::string EnteredString)
+{
+	if (EnteredString.find('\n') == 0 || EnteredString.find(' ') == 0) {
+		cout << "You cannot start a name with a space." << endl << endl;
+		return true;
+	}
+	for (char Letter : EnteredString)
+	{
+		if (Letter == '\n' || Letter == ' ' || Letter == '\\' || Letter == '/') {
+			cout << "You cannot enter more than one space or invalid characters such as \\ or /." << endl << endl;
+			return true;
+		}
+	}
+
+	return false;
 }
